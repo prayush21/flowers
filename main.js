b48048928bc40264439c2ac6233f97e19bf4577b
prayush21 <prayushdave@gmail.com>
2024-06-12 10:15:23 +0530

    Toggle Audio on Touch/Click

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 const backgroundAudio = new Audio("kesariya.mp3");
-
+let progressBarFill;
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    const progressBar = document.getElementById("progressBar");
+    progressBarFill = document.createElement("div");
+    progressBarFill.id = "progressBarFill";
+    progressBar.appendChild(progressBarFill);
     clearTimeout(c);
   }, 1000);
 };
 
-window.addEventListener("click", () => {
-  backgroundAudio.play();
-});
+function togglePlayPause() {
+  if (backgroundAudio.paused) {
+    backgroundAudio.play();
+  } else {
+    backgroundAudio.pause();
+  }
+}
 
-window.addEventListener("touchstart", () => {
-  backgroundAudio.play();
-});
+// Event listener for touchstart to toggle play/pause
+window.addEventListener("touchstart", togglePlayPause);
+window.addEventListener("click", togglePlayPause);
 
-// window.onload = function () {
-//   backgroundAudio.play();
-// };
-
-// backgroundAudio.play(); // Start playback (optional)
 backgroundAudio.loop = true; // Loop the audio (optional)
+
+backgroundAudio.addEventListener("loadedmetadata", () => {
+  const duration = backgroundAudio.duration;
+  progressBarFill.style.width = "0%";
+});
+
+backgroundAudio.addEventListener("progress", () => {
+  const buffered = backgroundAudio.buffered;
+  if (buffered.length > 0) {
+    const bufferedEnd = buffered.end(buffered.length - 1);
+    const duration = backgroundAudio.duration;
+    const progress = (bufferedEnd / duration) * 100;
+    progressBarFill.style.width = progress + "%";
+  }
+});
 
 backgroundAudio.addEventListener("error", () => {
   console.error("Error loading background audio");
