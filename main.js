69ad30509d73e3fdff034bda568d6e50eae9a86b
prayush21 <prayushdave@gmail.com>
2024-06-12 08:35:04 +0530

    Add Audio

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+const backgroundAudio = new Audio("kesariya.mp3");
+
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+    // backgroundAudio.play();
     clearTimeout(c);
   }, 1000);
 };
+
+backgroundAudio.play(); // Start playback (optional)
+backgroundAudio.loop = true; // Loop the audio (optional)
+
+backgroundAudio.addEventListener("error", () => {
+  console.error("Error loading background audio");
+});
